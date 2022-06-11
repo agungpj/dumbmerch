b9a33fa9b076de3e9cca6133411a3ea971035a06
Agung Prasetya <agungprasetya1121@gmail.com>
2022-06-12 01:52:21 +0700

    seeder fix

--- index.js
+++ index.js
@@ -16,14 +16,13 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000", // we must define cors because our client and server have diffe
+    origin: process.env.CLIENT_URL, // we must define cors because our client and server have diffe
   },
 });
 
 // import socket function and call with parameter io
 require("./src/socket")(io);
 
-// const port = 5000;
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
@@ -32,6 +31,13 @@ app.use(cors());
 // Add endpoint grouping and router
 app.use("/api/v1/", router);
 app.use("/uploads", express.static("uploads"));
+
+app.get("/", function (req, res) {
+  res.send({
+    message: "Hello World",
+    CLIENT_URL: process.env.CLIENT_URL,
+  });
+});
 
 // change app to server
 server.listen(port, () => console.log(`Listening on port ${port}!`));
